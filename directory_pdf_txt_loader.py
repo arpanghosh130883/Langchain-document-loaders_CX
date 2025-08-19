4893da4fbe4ab04d60911eac05802af30703745f
Arpan Ghosh <70130254+arpanghosh130883@users.noreply.github.com>
2025-08-19 05:43:12 +0530

    Add files via upload

---
+++ directory_pdf_txt_loader.py
@@ -0,0 +1,22 @@
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader, TextLoader
+
+# Load PDFs
+pdf_loader = DirectoryLoader(
+    path='books',
+    glob='*.pdf',
+    loader_cls=PyPDFLoader
+)
+
+# Load TXT files
+txt_loader = DirectoryLoader(
+    path='books',
+    glob='*.txt',
+    loader_cls=TextLoader
+)
+
+# Combine the two loaders
+docs = list(pdf_loader.lazy_load()) + list(txt_loader.lazy_load())
+
+# Print metadata for each document
+for document in docs:
+    print(document.metadata)
