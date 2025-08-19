54583ebb53495a38d3a737d9037a5ae615074d20
Arpan Ghosh <70130254+arpanghosh130883@users.noreply.github.com>
2025-08-19 06:04:20 +0530

    Update directory_pdf_txt_loader.py

--- directory_pdf_txt_loader.py
+++ directory_pdf_txt_loader.py
@@ -1,3 +1,5 @@
+# pip install chardet charset-normalizer
+
 from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader, TextLoader
 
 # Load PDFs
@@ -12,7 +14,7 @@ txt_loader = DirectoryLoader(
     path='books',
     glob='*.txt',
     loader_cls=TextLoader,
-    loader_kwargs={"encoding": "utf-8", "autodetect_encoding": True}
+    loader_kwargs={"encoding": "utf-8", "autodetect_encoding": True} # If we have .txt files with different encoding like utf-8 and utf-16 , we need to remove the hardcoded "encoding": "utf-8" and keep only "autodetect_encoding": True 
 )
 
 # Combine the two loaders
@@ -21,4 +23,5 @@ docs = list(pdf_loader.lazy_load()) + list(txt_loader.lazy_load())
 # Print metadata for each document
 for document in docs:
     print(document.metadata)
+
 
